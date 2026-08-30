b8f178c8d87de744a91ff10e27843c8bb5694d9a
syafiqrn8-dev <syafiqrn8@gmail.com>
2026-08-30 14:37:49 +0700

    Array Python

---
+++ ArrayPython.py
@@ -0,0 +1,14 @@
+# Array 
+Daftar_Keluarga = ["Andi", "Budi", "Elma", "Jenny"]
+print(Daftar_Keluarga)
+print(Daftar_Keluarga[0])
+
+# Array dalam Array
+Data_Murid = [
+    ["Ardi", "X-C"],
+    ["Bensu", "XI-B"],
+    ["Clara", "XII-A"]
+    ]
+print(Data_Murid)
+print(Data_Murid[0])
+
