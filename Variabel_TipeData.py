b4ab39387f0a8fd82553cc9e0272dc46fe5a54f8
syafiqrn8-dev <syafiqrn8@gmail.com>
2026-08-30 14:19:25 +0700

    Variable & Data Type

---
+++ Variabel_TipeData.py
@@ -0,0 +1,23 @@
+# Hitung Body Mass Index
+
+height = 1.79
+weight = 68.7
+BMI = weight / height ** 2
+
+print(BMI)
+
+int = 5
+float = 5.5
+string = "Ini String ia menggunakan kutip" # panggilannya str, string + string == stringstring
+boolean = True # dan False
+
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Print new_savings
+print(new_savings)
---
+++ firstCode.py
@@ -0,0 +1,11 @@
+# Pennjumlahan
+print(4 + 5)
+
+# Pengurangan
+print(5 - 5)
+
+# Perkalian
+print(3 * 5)
+
+# Pembagian
+print(10 / 2)
