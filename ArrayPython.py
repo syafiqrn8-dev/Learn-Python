af4c7981f8c6861fb2f3a5a60dd7320a7fbf2a96
syafiqrn8-dev <syafiqrn8@gmail.com>
2026-08-30 16:14:32 +0700

    SubsettingArray: Pemanggilan/Pengambilan nilai dalam Array

--- ArrayPython.py
+++ ArrayPython.py
@@ -1,14 +1,17 @@
-# Array 
+# Array
 Daftar_Keluarga = ["Andi", "Budi", "Elma", "Jenny"]
 print(Daftar_Keluarga)
-print(Daftar_Keluarga[0])
+print(Daftar_Keluarga[0])  # Andi
+print(Daftar_Keluarga[1:3])  # Budi, Jenny
+print(Daftar_Keluarga[:2])  # Andi, Budi
+print(Daftar_Keluarga[1:])  # Budi, Elmma, Jenny
 
 # Array dalam Array
 Data_Murid = [
     ["Ardi", "X-C"],
     ["Bensu", "XI-B"],
     ["Clara", "XII-A"]
-    ]
+]
 print(Data_Murid)
-print(Data_Murid[0])
-
+print(Data_Murid[0])  # ["Ardi", "X-C"]
+print(Data_Murid[2][1])  # "XII-A"
---
+++ SubsettingArray.py
@@ -0,0 +1,19 @@
+# Cara memperoleh/mengambil data dari Array
+
+# Indeks
+Daftar_Keluarga = ["Andi", "Budi", "Elma", "Jenny"]
+print(Daftar_Keluarga)
+print(Daftar_Keluarga[0]) # Andi
+print(Daftar_Keluarga[1:3]) # Budi, Jenny
+print(Daftar_Keluarga[:2]) # Andi, Budi
+print(Daftar_Keluarga[1:]) # Budi, Elmma, Jenny
+
+# Indeks Spesifik di dalam Array
+Data_Murid = [
+    ["Ardi", "X-C"],
+    ["Bensu", "XI-B"],
+    ["Clara", "XII-A"]
+    ]
+print(Data_Murid)
+print(Data_Murid[0]) # ["Ardi", "X-C"]
+print(Data_Murid[2][1]) # "XII-A"
